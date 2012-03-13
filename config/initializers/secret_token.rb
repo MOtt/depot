08210678794ce01b4aa2a68905be9e76d8d9c8c4
Michael Ott <ottmic02@students.zhaw.ch>
2012-03-13 10:58:34 +0100

    Versuch Nr. 124234

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '1abe0c62400aa4d39d597e9c7186562e8c917c036516656dcc093156aefefe6a164056525eeb0f19b835098e166d54319a39e00c3eea3f9dbf4b9d519c7b874d'
