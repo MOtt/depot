a9c4306cb9a106b61af69d518d0313f74550452a
Michael Ott <ottmic02@students.zhaw.ch>
2012-06-08 01:52:04 +0200

    Kapitel 11

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Product.create(title: 'Programming Ruby 1.9',
+Product.create(title: 'Professionelle Webentwicklung mit Ruby on Rails 3',
                description:
-               %{<p>
-               Ruby is the fastest growing and most exciting dynamic language
-               out there. If you need to get working programs delivered fast,
-               you should add Ruby to your toolbox.
-               </p>},
-               image_url: 'ruby.jpg',
-               price: 49.956)
+               %{Test und Behaviour Driven Development mit RSpec und User Stories REST, AJAX mit jQuery, HAML und SASS Suchfunktionen integrieren, Deployment mit Capistrano Das deutsche Standardwerk zur neuen Version 3 Ruby on Rails hat sich in Windeseile zu einem beliebten Framework entwickelt. Jens-Christian Fischer richtet sich an ambitionierte Einsteiger und gibt ihnen einen praxisbezogenen Einstieg in die Entwicklung von Webanwendungen. Der Leser wird Schritt für Schritt durch das Erstellen einer vollständigen Webanwendung geführt, anhand derer alle Konzepte vermittelt werden. Der Autor legt insbesondere Wert auf den Einsatz moderner Techniken wie Test Driven Development, User Stories, REST, Capistrano und jQuery. So erhält der Leser ein umfassendes Handbuch zu Rails 3.},
+               image_url: 'http://media.buch.de/img-adb/18070584-00-00/professionelle_webentwicklung_mit_ruby_on_rails_3.jpg',
+               price: 64.50)
+
+
+Product.create(title: 'Rails 3 in Action',
+               description:
+               %{Rails makes getting an application up and running easy, but how can a developer ensure that it continues to function well? The answer is Test and Behavior-Driven Development. These Agile approaches, combined with the advantages of the latest software version, make Rails 3 a powerful development framework.},
+               image_url: 'http://media.buch.de/img-adb/29406805-00-00/rails_3_in_action.jpg',
+               price: 53.90)
